a995e7ee225ce4b035c010e5e1300f3977d39cb0
Erik <dev@covenant.cc>
2022-01-29 09:44:19 -0500

    Added working and borked examples to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,29 +2,36 @@ import { NhostAuthProvider, useNhostAuth } from '@nhost/react-auth'
 
 import { nhost } from './nhost'
 import '../styles/globals.scss'
-import Login from './Login'
+import Login from '../lib/components/Login'
+import { RequireAuth } from '../lib/components/RequireAuth'
 
 function MyApp({ Component, pageProps }) {
 
   const { isLoading, isAuthenticated } = useNhostAuth()
   const user = nhost.auth.getUser()
 
-  console.debug(`Loading: ${isLoading}`)
-  console.debug(`Authenticated: ${isAuthenticated}`)
-  console.debug(`Second Auth Check: ${nhost.auth.getAuthenticationStatus().isAuthenticated}`)
-  console.debug(`User: ${user}`)
+  console.log(`Loading: ${isLoading}`)
+  console.log(`Authenticated: ${isAuthenticated}`)
+  console.log(`User: ${user}`)
 
   return (
     <NhostAuthProvider nhost={nhost}>
+
+      {/* BORKED EXAMPLE */}
       {
-        // !isAuthenticated ?
-        // user === null ?
-        !nhost.auth.getAuthenticationStatus().isAuthenticated ?
-          <Login /> :
-          <Component {...pageProps} />
+        isAuthenticated ?
+          <Component {...pageProps} /> :
+          <Login />
       }
+
+      {/* WORKING EXAMPLE */}
+      {/* <RequireAuth>
+        <Component {...pageProps} />
+      </RequireAuth> */}
+
     </NhostAuthProvider>
   )
 }
 
 export default MyApp
+
